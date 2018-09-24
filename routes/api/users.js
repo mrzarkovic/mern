480ed7372fd86ac9dc078b09325e5a3c776b15bf
Milos Zarkovic <mzarkovicm@gmail.com>
2018-09-24 16:24:17 +0200

    add models

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Load User model
+const User = require('../../models/User');
 
 /**
  * @example	GET api/users/test
@@ -11,5 +16,80 @@ router.get('/test', (req, res) =>
     message: 'Users Works'
   })
 );
+
+/**
+ * @example	POST api/users/register
+ * @desc	Register a user
+ * @access	Public
+ */
+router.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    }
+
+    const avatar = gravatar.url(email, {
+      s: '200', // Size
+      r: 'pg', // Rating
+      d: 'mm' // Default
+    });
+
+    const newUser = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
+      });
+    });
+  });
+});
+
+/**
+ * @example	POST api/users/login
+ * @desc	Login User / Returning JWT
+ * @access	Public
+ */
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        // Create JWT payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+        // Sign token
+        jwt.sign(payload);
+      }
+
+      return res.status(400).json({ password: 'Password is incorrect' });
+    });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
